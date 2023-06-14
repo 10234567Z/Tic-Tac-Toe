fdc2ed117ce9f8eecb8a2eac3f4f179b4713cd18
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-06-14 18:56:48 +0530

    Record player value upon click on any cell element

--- javascript.js
+++ javascript.js
@@ -11,11 +11,18 @@ function gameboard() {
     }
     let getBoard = () => board;
 
+    let cellAvailability = (column, row, value) => {
+        let cellsAvailable = board.filter(row => row[column].getValue() === '').map(row => row[column]);
+
+        if (!cellsAvailable.length) return;
+        board[row][column].addValue(value);
+    }
+
     let printBoard = () => {
         let boardWithValues = board.map((rows) => rows.map((cell) => cell.getValue()))
     }
 
-    return { getBoard, printBoard }
+    return { getBoard, printBoard, cellAvailability }
 }
 
 
@@ -31,20 +38,62 @@ function cell() {
     return { addValue, getValue };
 }
 
+function gameController() {
+    let board = gameboard();
+
+    let players = [
+        {
+            value: 'X'
+        },
+        {
+            value: 'O'
+        }
+    ]
+
+    let activePlayer = players[0];
+
+    let getActivePlayer = () => activePlayer;
+
+    let playRound = (row, column) => {
+        board.cellAvailability(column,row, getActivePlayer().value)
+    }
+
+    board.printBoard();
+
+    return { getActivePlayer, playRound }
+}
 
 function UIController() {
     let gBoard = gameboard();
     let board = gBoard.getBoard();
     let boardElement = document.querySelector('.gameboard');
-
+    let game = gameController();
+    
     let updateScreen = () => {
-        board.forEach(row => row.forEach((cell, index) => {
-            let cellElement = document.createElement('button');
-            cellElement.classList.add('cell');
-            boardElement.appendChild(cellElement);
-        }))
+        boardElement.textContent = '';
+        board.forEach((row, rowIndex) => {
+            row.forEach((cell, colIndex) => {
+                let cellElement = document.createElement('button');
+                cellElement.classList.add('cell');
+                boardElement.appendChild(cellElement);
+                cellElement.innerHTML = cell.getValue();
+                cellElement.dataset.column = colIndex;
+                cellElement.dataset.row = rowIndex;
+            }
+            )
+        }
+        )
     }
 
+    boardElement.addEventListener('click' , e => {
+        let selectedColumn = e.target.dataset.column;
+        let selectedRow = e.target.dataset.row;
+
+        if(!selectedColumn || !selectedRow) return;
+
+        game.playRound(selectedRow,selectedColumn);
+        updateScreen();
+    })
 
     updateScreen();
 }
